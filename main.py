b1f62dc69de90358f055e66718a8dc1b3f3f6dd6
Cheaster121 <cheater1211@gmail.com>
2022-06-19 17:38:14 +0300

    Added send messagge function

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import sys
 import datetime
+import json
+import requests
 from PyQt6 import uic, QtCore, QtGui, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
     server_adress = "https://chatletti.ru"
-    Username = "Name1"
+    api = "/api/messenger/"
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('untitled.ui', self)
+        self.SendButton1.clicked.connect(self.SendButton1_clicked)
+
+    def SendButton1_clicked(self):
+        self.sendmessage()
+
+    def sendmessage(self):
+        Username = self.Username1.text()
+        Messagetext = self.InputText1.text()
+        Timestamp = str(datetime.datetime.today())
+        Recipient = self.Recipient1.text()
+        msg = f"{{\"Username\": \"{Username}\", \"Messagetext\": \"{Messagetext}\", \"Timestamp\": \"{Timestamp}\", \"Recipient\": \"{Recipient}\"}}"
+        print(msg)
+        url = self.server_adress + self.api + Username
+        data = json.loads(msg)
+        r = requests.post(url, json=data)
 
 
 
