9478c2607ab0e0b8abcb2e3302aef493fcc6ff34
Cheaster121 <cheater1211@gmail.com>
2022-06-19 17:05:30 +0300

    Added run function for UI

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import sys
+import datetime
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainWindow(QtWidgets.QMainWindow):
+    server_adress = "https://chatletti.ru"
+    Username = "Name1"
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('untitled.ui', self)
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
 
