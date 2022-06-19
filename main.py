e1fbefc061df28263d0852a4684ed3d354df80a6
Cheaster121 <cheater1211@gmail.com>
2022-06-19 19:06:50 +0300

    Added get message function (need to fix later)

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import sys
 import datetime
 import json
 import requests
-from PyQt6 import uic, QtCore, QtGui, QtWidgets
+from PyQt6 import uic, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
     server_adress = "https://chatletti.ru"
-    api = "/api/messenger/"
+    api = "/api/messenger"
+    rows = 0
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('untitled.ui', self)
         self.SendButton1.clicked.connect(self.SendButton1_clicked)
+        self.RefreshButton.clicked.connect(self.refresh)
 
     def SendButton1_clicked(self):
         self.sendmessage()
@@ -22,10 +24,40 @@ class MainWindow(QtWidgets.QMainWindow):
         Timestamp = str(datetime.datetime.today())
         Recipient = self.Recipient1.text()
         msg = f"{{\"Username\": \"{Username}\", \"Messagetext\": \"{Messagetext}\", \"Timestamp\": \"{Timestamp}\", \"Recipient\": \"{Recipient}\"}}"
-        print(msg)
-        url = self.server_adress + self.api + Username
+        url = self.server_adress + self.api
         data = json.loads(msg)
-        r = requests.post(url, json=data)
+        requests.post(url, json=data)
+        msgtext = f"{Username}({Timestamp}): {Messagetext}"
+        self.listWidget1.insertItem(self.rows, msgtext)
+        self.rows += 1
+
+    def getmessage(self):
+        Username = self.Username1.text()
+        url = self.server_adress + self.api + "/" + Username
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except HTTPError:
+            return None
+        except:
+            return None
+        else:
+            text = response.text
+            return text
+
+    def refresh(self): #need to fix
+        msg = self.getmessage()
+        try:
+            messages = json.loads(msg)
+            #here is bug
+            Username = messages["Username"]
+            Messagetext = messages["Messagetext"]
+            Timestamp = messages["Timestamp"]
+            msgtext = f"{Username}({Timestamp}): {Messagetext}"
+            self.listWidget1.insertItem(self.rows, msgtext)
+            self.rows += 1
+        except:
+            print("Ooops")
 
 
 
